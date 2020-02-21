107fe31dd53b4e9ff536c933820c9b9db29ecedc
Lövlie <fredriklovlie@gmail.com>
2020-02-21 16:39:31 +0100

    StringParsing & Spawning

---
+++ Assets/scripts/Word.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Word : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro Text = null;
+
+    [SerializeField] private BoxCollider2D Collider = null;
+
+    void Start()
+    {
+        if(Text == null)
+        {
+            Text = GetComponent<TextMeshPro>();
+        }
+        if(Collider == null)
+        {
+            Collider = GetComponent<BoxCollider2D>();
+        }
+        //Collider.bounds = Text.textBounds;
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/scripts/WordSpawner.cs
+++ Assets/scripts/WordSpawner.cs
@@ -1,20 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WordSpawner : MonoBehaviour
 {
+    [SerializeField] private BoxCollider2D SpawnBounds = null;
 
+    [SerializeField] private string[] Sentences = null;
 
-    // Start is called before the first frame update
+    [SerializeField] private TextMeshPro TextPrefab;
+
+    private List<string> WordPile = new List<string>();
+    private List<TextMeshPro> Words = new List<TextMeshPro>();
     void Start()
+    {
+        if(SpawnBounds == null)
+        {
+            SpawnBounds = GetComponent<BoxCollider2D>();
+        }
+
+        foreach(string s in Sentences)
+        {
+            ParseSentence(s);
+        }
+
+        foreach(string s in WordPile)
+        {
+            Debug.Log("Logged word: " + s);
+            TextMeshPro newWord = Instantiate(TextPrefab);
+            newWord.text = s;
+            //newWord.gameObject.SetActive(false);
+        }
+
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ParseSentence(string pString)
     {
-        
+        List<char> letters = new List<char>();
+
+        int lastSpace = 0;
+
+        for(int i = 0; i < pString.Length; i++)
+        {
+            if(pString[i] == ' '
+                || i == pString.Length)
+            {
+                string newWord = pString.Substring(lastSpace, i - lastSpace);
+                
+                WordPile.Add(newWord);
+                
+                letters.Clear();
+                lastSpace = i+1;
+            }
+            else
+            {
+                letters.Add(pString[i]);
+            }
+        }
+        string lastWord = pString.Substring(lastSpace, pString.Length - lastSpace);
+        WordPile.Add(lastWord);
     }
 }
