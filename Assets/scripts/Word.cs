37a205ac3fc69850e9afd5cb67c2611f6400643d
Lövlie <fredriklovlie@gmail.com>
2020-02-25 14:50:17 +0100

    Word Spawning

--- Assets/scripts/Word.cs
+++ Assets/scripts/Word.cs
@@ -9,6 +9,11 @@ public class Word : MonoBehaviour
 
     [SerializeField] private BoxCollider2D Collider = null;
 
+    [SerializeField] private float MovementSpeed = 5.0f;
+
+    [HideInInspector] public int PointValue = 0;
+    private Vector3 HeadingDirection = Vector3.zero;
+
     void Start()
     {
         if(Text == null)
@@ -19,11 +24,33 @@ public class Word : MonoBehaviour
         {
             Collider = GetComponent<BoxCollider2D>();
         }
-        //Collider.bounds = Text.textBounds;
+        Destroy(this, 10);
     }
 
     void Update()
     {
-        
+        if(HeadingDirection != Vector3.zero)
+        {
+            transform.position += HeadingDirection * MovementSpeed * Time.deltaTime;
+        }
+    }
+
+    public void SetDirection(Direction pDirection)
+    {
+        switch(pDirection)
+        {
+            case Direction.NORTH:
+                HeadingDirection = Vector3.down;
+                break;
+            case Direction.EAST:
+                HeadingDirection = Vector3.left;
+                break;
+            case Direction.SOUTH:
+                HeadingDirection = Vector3.up;
+                break;
+            case Direction.WEST:
+                HeadingDirection = Vector3.right;
+                break;
+        }
     }
 }
--- Assets/scripts/WordSpawner.cs
+++ Assets/scripts/WordSpawner.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+public enum Direction
+{
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}
+
+[System.Serializable]
+public class SentenceWrapper
+{
+    [SerializeField] public string Sentence;
+    [SerializeField] public int PointValue;
+}
+
 public class WordSpawner : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D SpawnBounds = null;
+    [SerializeField] private BoxCollider2D[] SpawnZones = null;
 
-    [SerializeField] private string[] Sentences = null;
+    [SerializeField] private SentenceWrapper[] Sentences = null;
 
-    [SerializeField] private TextMeshPro TextPrefab;
+    [SerializeField] private TextMeshPro TextPrefab = null;
+
+    [SerializeField] private float SpawnDelay = 0.5f;
+
+    private float SpawnTimer = 0;
+
 
     private List<string> WordPile = new List<string>();
-    private List<TextMeshPro> Words = new List<TextMeshPro>();
     void Start()
     {
         if(SpawnBounds == null)
@@ -20,24 +40,46 @@ public class WordSpawner : MonoBehaviour
             SpawnBounds = GetComponent<BoxCollider2D>();
         }
 
-        foreach(string s in Sentences)
+        foreach(SentenceWrapper s in Sentences)
         {
-            ParseSentence(s);
+            ParseSentence(s.Sentence);
         }
 
-        foreach(string s in WordPile)
-        {
-            Debug.Log("Logged word: " + s);
-            TextMeshPro newWord = Instantiate(TextPrefab);
-            newWord.text = s;
-            //newWord.gameObject.SetActive(false);
-        }
+        //foreach(string s in WordPile)
+        //{
+        //    Debug.Log("Logged word: " + s);
+        //    TextMeshPro newWord = Instantiate(TextPrefab);
+        //    newWord.text = s;
+        //}
 
     }
 
     void Update()
     {
-        
+        SpawnTimer -= Time.deltaTime;
+
+        if(SpawnTimer <= 0)
+        {
+            SpawnWord();
+        }
+    }
+
+    private void SpawnWord()
+    {
+        Direction randomDirection = (Direction)Random.Range(0, 4);
+
+        Vector3 spawnPos;
+
+        spawnPos.x = Random.Range(SpawnZones[(int)randomDirection].bounds.min.x, SpawnZones[(int)randomDirection].bounds.max.x);
+        spawnPos.y = Random.Range(SpawnZones[(int)randomDirection].bounds.min.y, SpawnZones[(int)randomDirection].bounds.max.y);
+
+        spawnPos.z = transform.position.z;
+
+        TextMeshPro newWord = Instantiate(TextPrefab, spawnPos, Quaternion.identity);
+        newWord.GetComponent<Word>().SetDirection(randomDirection);
+        newWord.text = WordPile[Random.Range(0, WordPile.Count)];
+
+        SpawnTimer = SpawnDelay;
     }
 
     private void ParseSentence(string pString)
